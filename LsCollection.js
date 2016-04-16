61e058cf5ab30e9df583ef1da21d88bbb67fcf9a
Jason Jones <jason@2jdevelopment.com>
2016-04-15 20:57:19 -0700

    Add implementation of LsCollection object

---
+++ LsCollection.js
@@ -0,0 +1,80 @@
+"use strict";
+var LsCollection = (function () {
+    function LsCollection(id) {
+        var _this = this;
+        // Map Passthru
+        this.ForEach = function (callbackfn) {
+            for (var key in _this._col) {
+                callbackfn(_this._col[key], key);
+            }
+        };
+        this._lsId = id;
+        this._col = {};
+        document.addEventListener('pause', this);
+        document.addEventListener('resume', this);
+    }
+    LsCollection.prototype.Dispose = function () {
+        document.removeEventListener('pause', this);
+        document.removeEventListener('resume', this);
+    };
+    Object.defineProperty(LsCollection.prototype, "LsId", {
+        get: function () { return this._lsId; },
+        set: function (value) { this._lsId = value; },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(LsCollection.prototype, "HashFunc", {
+        set: function (value) { this._hashFunc = value; },
+        enumerable: true,
+        configurable: true
+    });
+    // Add and update are identical at this time.
+    LsCollection.prototype.Add = function (item) {
+        this.Update(item);
+    };
+    // Add and update are identical at this time.
+    LsCollection.prototype.Update = function (item) {
+        var id = this._hashFunc(item);
+        this._col[id] = item;
+    };
+    LsCollection.prototype.Remove = function (id) {
+        var ret = this._col[id];
+        delete this._col[id];
+        return ret;
+    };
+    LsCollection.prototype.Get = function (id) {
+        return this._col[id];
+    };
+    Object.defineProperty(LsCollection.prototype, "Entries", {
+        get: function () { return this._col; },
+        enumerable: true,
+        configurable: true
+    });
+    //// Private functions
+    LsCollection.prototype.serialize = function () {
+        return JSON.stringify(this._col);
+    };
+    LsCollection.prototype.deserialize = function (json) {
+        return JSON.parse(json);
+    };
+    /// Event Handlers
+    LsCollection.prototype.handleEvent = function (ev) {
+        switch (ev.type) {
+            case 'pause':
+                this.onAppPause();
+                break;
+            case 'resume':
+                this.onAppResume();
+                break;
+        }
+    };
+    LsCollection.prototype.onAppPause = function () {
+        localStorage.setItem(this.LsId, this.serialize());
+    };
+    LsCollection.prototype.onAppResume = function () {
+        this._col = this.deserialize(localStorage.getItem(this.LsId));
+    };
+    return LsCollection;
+}());
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = LsCollection;
--- LsCollection.ts
+++ LsCollection.ts
@@ -1,5 +1,78 @@
 
+export default class LsCollection<T> {
+    constructor(id: string) {
+        this._lsId = id;
+        this._col = {};
 
-function thingy() {
-    console.log('thingy');
+        document.addEventListener('pause', this);
+        document.addEventListener('resume', this);
+    }
+    public Dispose() {
+        document.removeEventListener('pause', this);
+        document.removeEventListener('resume', this);
+    }
+
+    private _col: Object;
+
+    private _lsId: string;
+    public get LsId() { return this._lsId; }
+    public set LsId(value: string) { this._lsId = value; }
+
+    private _hashFunc: (T) => string;
+    public set HashFunc(value: (T) => string) { this._hashFunc = value; }
+
+    // Add and update are identical at this time.
+    public Add(item: T): void {
+        this.Update(item);
+    }
+
+    // Add and update are identical at this time.
+    public Update(item: T): void {
+        var id = this._hashFunc(item);
+        this._col[id] = item;
+    }
+
+    public Remove(id: string): T {
+        var ret = this._col[id];
+        delete this._col[id];
+
+        return ret;
+    }
+
+    public Get(id: string): T {
+        return this._col[id];
+    }
+
+    // Map Passthru
+    public ForEach = (callbackfn: (value: T, index: string) => void) => {
+        for(var key in this._col) {
+            callbackfn(this._col[key], key);
+        }
+    };
+    public get Entries() { return this._col; }
+
+    //// Private functions
+    private serialize(): string {
+        return JSON.stringify(this._col);
+    }
+    private deserialize(json: string): Object {
+        return JSON.parse(json);
+    }
+
+    /// Event Handlers
+    public handleEvent(ev: Event) {
+        switch(ev.type) {
+            case 'pause': this.onAppPause();break;
+            case 'resume': this.onAppResume();break;
+        }
+    }
+
+    private onAppPause() {
+        localStorage.setItem(this.LsId, this.serialize());
+    }
+    private onAppResume() {
+        this._col = this.deserialize(localStorage.getItem(this.LsId));
+    }
+
 }
+
