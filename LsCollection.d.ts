f645899e17775e71e4d86c2c43abe6d36c993963
Jason Jones <jason@2jdevelopment.com>
2016-04-15 21:31:42 -0700

    Add missing type declaration file

---
+++ LsCollection.d.ts
@@ -0,0 +1,20 @@
+export default class LsCollection<T> {
+    constructor(id: string);
+    Dispose(): void;
+    private _col;
+    private _lsId;
+    LsId: string;
+    private _hashFunc;
+    HashFunc: (T) => string;
+    Add(item: T): void;
+    Update(item: T): void;
+    Remove(id: string): T;
+    Get(id: string): T;
+    ForEach: (callbackfn: (value: T, index: string) => void) => void;
+    Entries: Object;
+    private serialize();
+    private deserialize(json);
+    handleEvent(ev: Event): void;
+    private onAppPause();
+    private onAppResume();
+}
